d914d729d7c6d634ce922f94be51213755062054
massii13 <djemouai-massinissa@hotmail.fr>
2018-03-21 15:34:13 +0100

    ajout jsp1 et jsp2

---
+++ src/projet1jee/Servlet1.java
@@ -0,0 +1,36 @@
+package projet1jee;
+
+import java.io.IOException;
+//import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet1 extends HttpServlet {
+	
+	public void doGet (HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		
+//		response.setContentType("text/html");
+//		response.setCharacterEncoding( "UTF-8" );
+//		PrintWriter out = response.getWriter();
+//		out.println("<!DOCTYPE html>");
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<meta charset=\"utf-8\" />");
+//		out.println("<title>Test</title>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		out.println("<p>Ceci est une page générée depuis une servlet.</p>");
+//		out.println("</body>");
+//		out.println("</html>");
+		String message = "veuillez remplir"; 
+		request.setAttribute( "test", message );
+
+		this.getServletContext().getRequestDispatcher("/WEB-INF/jsp1.jsp").forward( request, response );
+		
+	}
+
+		
+}
